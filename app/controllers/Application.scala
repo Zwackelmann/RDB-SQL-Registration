3c4e35386776115d67c467644d16049e6524811f
Simon Barthel <simon@welt-erobern.de>
2013-10-05 20:01:27 +0200

    init

---
+++ app/controllers/Application.scala
@@ -0,0 +1,55 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import model._
+
+object Application extends Controller {
+  val matNoRe = """\d{7}""".r
+  
+  val registrationForm = Form(
+    mapping(
+      "matriculationnumber" -> text.verifying("no valid matriculation number", matNo => matNo match {
+        case matNoRe() => true
+        case _ => false
+      }),
+      "firstname" -> text,
+      "lastname" -> text,
+      "email" -> email,
+      "pick1" -> number,
+      "pick2" -> number,
+      "pick3" -> number,
+      "attend_to_rdb" -> boolean,
+      "attend_to_sql" -> boolean,
+      "teammate" -> text.verifying("no valid matriculation number", matNo => matNo match {
+        case matNoRe() => true
+        case "" => true
+        case _ => false
+      })
+    )(RegistrationData.apply)(RegistrationData.unapply).verifying("Group picks must not be equal", regData => regData match {
+      case RegistrationData(_, _, _, _, p1, p2, p3, _, _, _) => List(p1, p2, p3).distinct.size == 3
+      case _ => false
+    })
+  )
+
+  def index = Action {
+    Ok(views.html.index(RDBGroup.list(), registrationForm)) 
+  }
+  
+  def register = Action { implicit request => 
+    registrationForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html.index(RDBGroup.list(), formWithErrors)),
+      value => {
+        try {
+            value.save()
+            Ok(value.toString)
+        } catch {
+            case(th: Throwable) => BadRequest(views.html.index(RDBGroup.list(), registrationForm))
+        }
+      }
+    )
+  } 
+}
---
+++ app/model/Model.scala
@@ -0,0 +1,49 @@
+package model
+
+import play.api.db._
+import play.api.Play.current
+import scala.collection.mutable.ListBuffer
+
+case class RDBGroup(val number: Int, val regularDate: String, val hiwiName: Option[String])
+
+object RDBGroup {
+    def list() = {
+        DB.withConnection { conn => 
+            val groupsResultSet = conn.createStatement().executeQuery("SELECT nr, regular_date, hiwi_name FROM rdb_group");
+            
+            val groups = new ListBuffer[RDBGroup]();
+            while(groupsResultSet.next()) {
+                val number = groupsResultSet.getInt(1)
+                val regularDate = groupsResultSet.getString(2)
+                val hiwiName = groupsResultSet.getString(3)
+
+                groups += RDBGroup(number, regularDate, if(hiwiName == null) None else Some(hiwiName))
+            }
+
+            groups.toList
+        }
+    }
+}
+
+case class RegistrationData(val matriculationNumber: String, val firstname: String, val lastname: String, val email: String, val pick1: Int, pick2: Int, pick3: Int, val attendToRDB: Boolean, val attendToSQL: Boolean, val teammate: String) {
+
+
+    def save() {
+        DB.withConnection { conn =>
+            val insertRegistrationStmt = conn.prepareStatement("INSERT INTO registration (matriculationnumber, firstname, lastname, email, pick1, pick2, pick3, attend_to_rdb, attend_to_sql, teammate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+
+            insertRegistrationStmt.setString(1, matriculationNumber)
+            insertRegistrationStmt.setString(2, firstname)
+            insertRegistrationStmt.setString(3, lastname)
+            insertRegistrationStmt.setString(4, email)
+            insertRegistrationStmt.setInt(5, pick1)
+            insertRegistrationStmt.setInt(6, pick2)
+            insertRegistrationStmt.setInt(7, pick3)
+            insertRegistrationStmt.setString(8, if(attendToRDB) "t" else "f")
+            insertRegistrationStmt.setString(9, if(attendToSQL) "t" else "f")
+            insertRegistrationStmt.setString(10, if(teammate.length() == 0) null else teammate)
+            
+            insertRegistrationStmt.executeUpdate();
+        }
+    }
+}
